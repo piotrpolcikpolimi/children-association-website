7bf4c537e9e4074e30632b38a26f7249d61f2f6f
Wiking <p.polcik1@gmail.com>
2020-05-06 16:46:59 +0200

    Finished Persons page

--- public/assets/components/Volounteer-small/volounteer-small.js
+++ public/assets/components/Volounteer-small/volounteer-small.js
@@ -1,1 +1,15 @@
-class VolounteerSmall extends BaseComponent {}
+class VolounteerSmall extends BaseComponent {
+
+    getYear = () => {
+        return new Date(this.data.date_joined).getFullYear();
+    };
+
+    render = () => {
+        if (this.data.hasOwnProperty('date_joined')) {
+            this.data.date_joined = this.getYear();
+        }
+        return super.render();
+    }
+
+
+}
