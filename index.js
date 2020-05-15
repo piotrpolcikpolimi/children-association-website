92a797eaedcfa7b141918bea673d24e3fd10848f
Wiking <p.polcik1@gmail.com>
2020-05-15 12:51:52 +0200

    modified ports

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 8080;
+var serverPort = process.env.PORT || 8080;
 let { setupDataLayer } = require('./service/DataLayer');
 let serveStatic = require('serve-static');
 
