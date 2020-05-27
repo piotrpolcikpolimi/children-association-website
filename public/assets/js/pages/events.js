15e37b1c9e8e79bf724d356b1106c6644755f560
Wiking <p.polcik1@gmail.com>
2020-05-27 07:02:04 +0200

    Added pagination

--- public/assets/js/pages/events.js
+++ public/assets/js/pages/events.js
@@ -1,3 +1,5 @@
+let pagination;
+
 const parseEvent = (data) => {
     return {
         id: data.id,
@@ -37,19 +39,19 @@ const setLocationInfo = (locations) => {
 }
 
 (async () => {
-
+    const limit = 4;
     const queryParams = new URLSearchParams(window.location.search);
     let eventsData, eventsTemplate;
 
     // Fetch data
     if (queryParams.get('country')) {
-        eventsData = await global.fetchData('/events', `offset=0&limit=8&country=${queryParams.get('country')}`);
+        eventsData = await global.fetchData('/events', `offset=0&limit=${limit}&country=${queryParams.get('country')}`);
         setLocationInfo([{ class: 'inactive',text: 'Event',href: './events.html'},{class: 'inactive',text: 'Events By Country',href: './bycountry.html'},{class: 'active',text: queryParams.get('country'),}])
     } else if (queryParams.get('month')) {
-        eventsData = await global.fetchData('/events', `offset=0&limit=8&month=${queryParams.get('month')}`);
+        eventsData = await global.fetchData('/events', `offset=0&limit=${limit}&month=${queryParams.get('month')}`);
         setLocationInfo([{ class: 'inactive',text: 'Event', href: './events.html'},{class: 'inactive',text: 'Events By Month',href: './bymonth.html'},{class: 'active',text: getMonthName(queryParams.get('month'))}])
     } else {
-        eventsData = await global.fetchData('/events', 'offset=0&limit=8');
+        eventsData = await global.fetchData('/events', `offset=0&limit=${limit}`);
     }
 
     // Get data JSON, fetch template
@@ -64,6 +66,11 @@ const setLocationInfo = (locations) => {
 
     // insert elments into DOM
     global.appendChildrenToSlot(global.getTemplateSlot('events'), events);
+
+    pagination = new Pagination('/events', 'events', limit, eventsData.meta.total_number, global.getTemplateSlot('events'), EventLarge.prototype.constructor, ['event-large', ['id', 'thumbnail', 'name', 'thumbnail_desc', 'location', 'flag'], eventsTemplate], parseEvent);
+    pagination.initialize().then(() => {
+        pagination.render();
+    })
 
     $(document).ready(setTimeout(() => {global.loaded()},300));
 })();
