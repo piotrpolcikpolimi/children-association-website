bb47f3960cf4656935dacd77abfad75dc40638e0
Wiking <p.polcik1@gmail.com>
2020-06-03 08:54:05 +0200

    Modified donation form, linked to the api

--- public/assets/js/pages/donate.js
+++ public/assets/js/pages/donate.js
@@ -2,3 +2,40 @@
     $(document).ready(setTimeout(() => {global.loaded()},300));
 })();
 
+const sendForm = (e) => {
+    e.preventDefault();
+    const formData = $('#donation-form').serializeArray();
+    const data = new URLSearchParams();
+    let msg = '';
+    let amount;
+
+    formData.forEach((el, i) => {
+        if (el.value && el.name !== 'amount') {
+            msg += `${el.name}: ${el.value} `;
+        }
+        if (el.name === 'amount') {
+            amount = el;
+        }
+    });
+
+    data.append('amount', amount.value);
+    data.append('message', msg);
+
+    global.postData('donations', data, 'application/x-www-form-urlencoded').then(
+        resp => {
+            global.updateForm(resp, 'Thank you for donating!', 'donation-form');
+        }
+    )
+};
+
+const setAmountValue = (value) => {
+        if (!value) {
+            $('#donationField')[0].value = '';
+            return;
+        }
+        $('#donationField')[0].value = value;
+}
+
+
+
+
